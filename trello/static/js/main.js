4b80f47484799897486f7f5c3daba33b54c3e3a6
Madarga <madarga@email.com>
2019-08-22 21:09:01 +0800

    Cant get the JsonResponse from django view

--- trello/static/js/main.js
+++ trello/static/js/main.js
@@ -21,80 +21,93 @@ function add_board(){
 
     function createBoard() {
         $('#board-form').on('submit', function(e){
-        console.log('submit');
-        e.preventDefault();
+            e.preventDefault();
+            $.ajax({
+                url: $(this).attr('action'),
+                data: $(this).serialize(),
+                method: 'POST'
+            }).done(function(data){
+                $('#modal').modal('hide');
+                window.location.href = '/board/'+data.board;  
+            }).fail(function(xhr, data){
+                $('.error').show();
+            });        
+        });
 
+    }
+}
+
+function clist(){
+    $.ajax({
+        method: 'get',
+        url: '/board/'
+    }).done(function(response){
+        vlist();
+        console.log('HA');
+    });
+}
+
+function vlist(){
+    $('#list-form').on('submit', function(e){
+        console.log('submit list');
         $.ajax({
             url: $(this).attr('action'),
             data: $(this).serialize(),
             method: 'POST'
         }).done(function(data){
-            $('#modal').modal('hide');
-            window.location.href = '/board/'+data.board;  
-            console.log(data)
-        }).fail(function(err){
-            alert(err)
-        })
-    });
+            console.log(data);
+            //window.location.href = '/create-list/';
+            $('#list-form').attr('');
+            //$("body").find('.container').html(data)
+            $("body").find('.container').append(data.board_list)
+        }).fail(function(xhr, data){
 
-    }
-}
-
-
-function addList(){
-    $('#list-form').on('click', function(){
-        console.log("GET2.....");
-        $.ajax({
-            url: $(this).attr('action'),
-            method: 'get',
-        }).done(function(response){
-            console.log("GET.....");
-            createList();
-        })
-    });
-}
-
-
-function viewList(){
-    $.ajax({
-        url: '/create-list/',
-        method: 'get',
-    }).done(function(response){
-
+        });
     })
 }
 
-function createList(){
-    $('#list-form').on('submit', function(e){
-        console.log('submit formlist');
-
-        // $.ajax({
-        //     url: $(this).attr('action'),
-        //     data: $(this).serialize(),
-        //     method: 'POST'
-        // }).done(function(data){   
-        // }).fail(function(err){
-        //     alert(err)
-        //     console.log(error)
-        // })
-    });
+function addList(){
+     $.ajax({
+         url: '/create-list/',
+         method: 'get',
+     }).done(function(response){
+         $("body").find('.container').html(response)
+         updateList();
+     })
 }
 
-function editListTitle(){
-    $('.ltitle').click(function(e){
-        var edited_list = $(this).attr('contenteditable', 'true');
-        $(this).attr('focus');
 
-        $('.ltitle[contenteditable').keypress(function(e){
-            if(e.keyCode == "13"){
-                $(edited_list).blur();
-            }
-        });
-    });
+// function updateList(){
+//     $('#b_list').click(function(e){
+//         var edit_list = $(this).attr('contenteditable', 'true');
+//         $(this).attr('focus');
+
+//         $('#b_list[contenteditable]').keypress(function(e){
+//             if(event.keyCode == "13"){
+//                 $(edit_list).blur();
+//                 console.log('HA')
+//             }
+//         });
+//     });
+// }
+
+function updateList(){
+    $('h5').click(function(e){
+        var data = $('#b-list').val();
+        console.log(data);
+            var edit_list = $(this).attr('contenteditable', 'true');
+            $(this).attr('focus');
+        $.ajax({
+            url: '/create-list/',
+            method: 'get',
+        }).done(function(response){
+
+        })
+    })
 }
 
 $(document).ready(function (){
     add_board();
-    addList();
-    editListTitle();
+    //addList();
+    //updateList();
 });
