744bc82f78557657c4471afd3802ac440c929345
Madarga <madarga@email.com>
2019-08-23 20:34:55 +0800

    Return error if username is invalid

--- trello/forms.py
+++ trello/forms.py
@@ -1,41 +1,60 @@
 from django import forms
 from .models import Board, List
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
-class SignupForm(forms.Form):
+class SignUpForm(forms.ModelForm):
+    """
+    Creates form for Sign Up. 
+    """
+    
     username = forms.CharField(max_length=30, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    email = forms.CharField(max_length=30, widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(max_length=30, widget=forms.EmailInput(attrs={'class': 'form-control'}))
     first_password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
     second_password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
 
-    
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'first_password', 'second_password')
+
     def clean_email(self):
         email = self.cleaned_data['email']
         user = User.objects.filter(email=email)
         if user.exists():
             raise forms.ValidationError('Email already taken')
         return email
+    
+    def clean(self):
+        cleaned_data = super().clean()
+        first_password = cleaned_data.get("first_password")
+        second_password = cleaned_data.get("second_password")
 
-    def clean_password(self):
-        first_password = self.cleaned_data['first_password']
-        second_password = self.cleaned_data['second_password']
         if first_password != second_password:
-            raise forms.ValidationError('Password mismatched')
-        
-        return first_password
+            raise forms.ValidationError(
+                self.add_error('second_password', "Passwords are mismatched!")
+            )
 
     def save(self, commit=True):
-        username = self.cleaned_data['username']
-        email = self.cleaned_data['email']
-        password = self.cleaned_data['first_password']
-        user = User.objects.create_user(username, email, password)
-        user.save()
-        return user
+        user = super().save(commit=False)
+        if commit:
+            user.set_password(self.cleaned_data.get('first_password'))
+            user.save()
+        return user 
 
 
 class LoginForm(forms.Form):
     username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
     password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        user = User.objects.filter(username=username)
+        if user.exists():
+            pass 
+        else:
+            raise forms.ValidationError('Invalid Username')
+
+        return username
 
 
 class AddBoardTitleForm(forms.ModelForm):
