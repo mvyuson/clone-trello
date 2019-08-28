675724ed8d51c445f4b5b90b4fbff4702872bebf
Madarga <madarga@email.com>
2019-08-28 18:16:45 +0800

    Added card_description field in Card model

--- trello/models.py
+++ trello/models.py
@@ -9,8 +9,10 @@ from datetime import datetime
 class Card(models.Model):
     board_list = models.ForeignKey('List', on_delete=models.CASCADE)
     card_title = models.CharField(max_length=200)
+    card_description = models.TextField()
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
+    archived = models.BooleanField(default=False)
 
     def __str__(self):
         return self.card_title
@@ -25,6 +27,7 @@ class List(models.Model):
     list_title = models.CharField(max_length=200)
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
+    archived = models.BooleanField(default=False)
 
     def __str__(self):
         return self.list_title
@@ -41,6 +44,7 @@ class Board(models.Model):
     title = models.CharField(max_length=200)
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True, editable=True)
+    archived = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
