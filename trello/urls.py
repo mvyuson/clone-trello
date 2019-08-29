aa60034e030f68193749a5671f036b2dfef233bd
Madarga <madarga@email.com>
2019-08-29 18:57:35 +0800

    Added update, archive, and  invite member urls

--- trello/urls.py
+++ trello/urls.py
@@ -9,17 +9,23 @@ from trello.views import (
         BoardView,
         CreateBoardView,
         AddCardView,
-        UpdateBoard,
-        DeleteBoardView,
-        UpdateListView,
-        DeleteListView,
         CardDescriptionView,
+        UpdateBoard,
+        UpdateListView,
+        DeleteBoardView,
+        DeleteCardView,
+        DeleteListView,
         ArchiveView,
+        BoardArchiveView,
+        ListArchiveView,
+        CardArchiveView,
+        InviteMemberView,
+
 )
 from .models import Board
 from django.urls import path
 
-urlpatterns = [
+urlpatterns = [  
     path('signup/', SignUpView.as_view(), name='signup'),
     path('login/', LoginView.as_view(), name='login'),
     path('password_reset/', auth_views.password_reset, name='password_reset'),
@@ -32,11 +38,19 @@ urlpatterns = [
     path('create-board/', CreateBoardView.as_view(), name='create-board'),
     path('board/<int:id>/', BoardView.as_view(), name='board'),
     path('board/<int:id>/list/', AddCardView.as_view(), name='add_card'),
+    path('description/<int:id>/', CardDescriptionView.as_view(), name='description'),
+
     path('board/<int:id>/edit-board/', UpdateBoard.as_view(), name='edit-board'),
-    path('board/<int:id>/delete-board/', DeleteBoardView.as_view(), name='delete-board'),
     path('board/<int:id>/edit-list/', UpdateListView.as_view(), name='edit-list'),
-    path('delete-list/<int:id>', DeleteListView.as_view(), name='delete-list'),
-    #path('board_archive/', ArchiveIndexView.as_view(model=Board, date_field="created_date")),
-    path('board_archive/', ArchiveView.as_view(), name='board_archive'),
-    path('description/<int:id>', CardDescriptionView.as_view(), name='description'),
+
+    path('board/<int:id>/delete-board/', DeleteBoardView.as_view(), name='delete-board'),
+    path('delete-list/<int:id>/', DeleteListView.as_view(), name='delete-list'),
+    path('delete-card/<int:id>/', DeleteCardView.as_view(), name='delete-card'),
+
+    path('archive/', ArchiveView.as_view(), name='archive'),
+    path('board-archive/<int:id>/', BoardArchiveView.as_view(), name='board-archive'),
+    path('list-archive/<int:id>/', ListArchiveView.as_view(), name='list-archive'),
+    path('card-archive/<int:id>/,', CardArchiveView.as_view(), name='card-archive'),
+
+    path('invite-member/<int:id>/', InviteMemberView.as_view(), name='invite-member'),
 ]
