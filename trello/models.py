483e4d12e5e88830e472cbb9e755276905496e31
Madarga <madarga@email.com>
2019-09-06 20:15:05 +0800

    Added docstrings

--- trello/models.py
+++ trello/models.py
@@ -7,6 +7,15 @@ from datetime import datetime
 
 
 class Card(models.Model):
+    """
+    'board_list' is the list the card is member of
+    'author' is the user who created the card
+    'card_title' is the title of the card
+    'created_date' automatically set everytime a new card is created
+    'updated_date automatically set everytime a card is updated
+    'archived' is set to True when a user want to archive the card
+    """
+
     board_list = models.ForeignKey('List', on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     card_title = models.CharField(max_length=200)
@@ -21,7 +30,12 @@ class Card(models.Model):
 
 class List(models.Model):
     """
-    Dapat niya tawagon ang pangalan sa board member na naghimo sa list
+    'board' is the board the list is member of
+    'author' is the user who created the list
+    'list_title' is the title of the list
+    'created_date' automatically set everytime a new list is created
+    'updated_date automatically set everytime a list is updated
+    'archived' is set to True when a user want to archive the list
     """
 
     board = models.ForeignKey('Board', on_delete=models.CASCADE)
@@ -54,8 +68,10 @@ class Board(models.Model):
 
 class BoardMembers(models.Model):
     """
-    Kwaa ang board.
-    Kwaa ang nagcreate sa board tapos ibutang kay members.
+    'board' is the board.
+    'members' are the member of the board
+    'deactivate' status of the member is equal to False when member is being added to the board.
+    'owner' is the user who created the board.
     """
 
     board = models.ForeignKey('Board', on_delete=models.CASCADE)
